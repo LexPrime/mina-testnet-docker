0806909da753b090725a986efb8767dcb62b37f1
lexprime <lexprime.official@gmail.com>
2024-05-20 10:52:39 +0300

    update lib2p2 keygen

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --interactive --tty --rm --volume $(pwd)/keys:/keys gcr.io/o1labs-192920/mina-daemon:${MINA_BP_VERSION} mina libp2p generate-keypair -privkey-path keys/
+source $(pwd)/.env
+
+docker run --interactive --rm --tty --volume $(pwd)/keys:/keys --entrypoint='mina' gcr.io/o1labs-192920/mina-daemon:${MINA_BP_VERSION} libp2p generate-keypair -privkey-path /keys/libkey
+
+sudo chown -R $USER:$USER keys
+sudo chmod 700 keys
+sudo chmod 600 keys/libkey
